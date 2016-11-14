31efcdaf68a47a6b6b65394dba8bb68f638ab1d3
jonick makau <r0591009@student.thomasmore.be>
2016-11-14 11:42:25 +0100

    inladen van de dependencies

--- projectDrones/main.js
+++ projectDrones/main.js
@@ -1,7 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Samen gewerkt met Jonick Makau, Galvani Makau, Jelle Van Loock en Kevin Aerts
+
+// inladen van de dependencies
+var express = require('express'); // eenvoudige webserver in node js
+var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
+var uuid = require('uuid');
 
 
