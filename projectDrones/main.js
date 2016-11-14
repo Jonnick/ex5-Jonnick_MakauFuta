95c438b4822292d054c5bfcf48fd95ea61ddc038
jonick makau <r0591009@student.thomasmore.be>
2016-11-14 11:43:44 +0100

    variabelen aanmaken

--- projectDrones/main.js
+++ projectDrones/main.js
@@ -5,4 +5,14 @@ var express = require('express'); // eenvoudige webserver in node js
 var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
 var uuid = require('uuid');
 
+var dal = require("./storage.js");
+//validatie inladen
+var validation = require("./validate.js");
+var validationlocaties = require("./validatelocaties.js");
+var validationaanwezigheden = require("./validateaanwezigheden.js");
+var validationbewegingen = require("./validatebewegingen.js");
 
+// aanmaken van de webserver variabele
+var app = express();
+// automatische json-body parsers van request MET media-type application/json gespecifieerd in de request.
+app.use(parser.json());
